47590e98beca9a1465c90aba3eadda1f9826323c
gerardo cerrillos machucho <128113091+GerardoCerrillos@users.noreply.github.com>
2023-07-05 18:29:09 -0600

    problemas tecnicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,90 @@
-import './App.css'
-import Darkmode from './components/Darkmode'
-
-
+import "./App.css";
+import Switch from "react-switch";
+import { useState, useEffect } from "react";
+import Clim from "./components/Clim";
+import axios from "axios";
 
 function App() {
+  // switch
+  const [checked, setChecked] = useState(false);
+  const handleChange = (nextChecked) => {
+    setChecked(nextChecked);
+  };
 
+  // weather data
+
+  const [coords, setCoords] = useState();
+  const [weather, setWeather] = useState();
+  const [temp, setTemp] = useState();
+  const [description, setDescription] = useState();
+
+  useEffect(() => {
+    const success = (pos) => {
+      const obj = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude,
+      };
+      setCoords(obj);
+    };
+
+    // llamada a la api del navegador para obtener ubicacion
+    navigator.geolocation.getCurrentPosition(success);
+  }, []);
+
+  //? llamada a la api del clima
+
+  useEffect(() => {
+    if (coords) {
+      const APIKEY = "58c46cddd658d854ede94ecbdf25ba56";
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords?.lat}&lon=${coords?.lon}&appid=${APIKEY}`;
+      axios
+        .get(URL)
+        .then((res) => {
+          const celsius = (res.data.main.temp - 273.15).toFixed(1);
+          const farenheit = ((celsius * 9) / 5 + 32).toFixed(1);
+          setTemp({ celsius, farenheit });
+          setWeather(res.data);
+          setDescription(res.data.weather[0].description);
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [coords]);
+
+  const update = (description) => {
+    let value = "";
+    switch (description) {
+      case "scattered clouds":
+      case "broken clouds":
+        value = cloud;
+        break;
+      case "clear sky":
+      case "few clouds":
+        value = clear;
+        break;
+    }
+    return value;
+  };
 
   return (
-    <>
-    <div>
-      <body>
-          <h1 className='h1'>weather app</h1>
-    <Darkmode/>
-      </body>
-    
+    <div className={checked ? "lightmode" : "darkmode"}>
+      <header className="header">
+        <h1 className="h1">weather app</h1>
+        <input type="text" placeholder="Busca una Ciudadd" />
+        <Switch
+          onChange={handleChange}
+          checked={checked}
+          className="react-switch"
+          uncheckedIcon={false}
+          checkedIcon={false}
+        />
+      </header>
+      <article className="forma1">
+        <Clim longitude={coords?.lon} latitude={coords?.lat} />
+      </article>
+      <button className="cambio"></button>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Clim.jsx
+++ src/components/Clim.jsx
@@ -1,6 +1,21 @@
-const Clim = () => {
-    return(
-        
-    )
 
-}
+const Clim = ({}) => {
+  return (
+    <div className="container">
+      <div className="clima1">
+        <h1>19</h1>
+        <ul>
+          <li>nublado</li>
+          <li>despegajo</li>
+          <li>presion</li>
+        </ul>
+      </div>
+        <img className="imgclim" src="/cloudy-day.png" alt="" />
+      <div className="ciudadConClima">
+        <h2>mexcio</h2>
+        <p>clima</p>
+      </div>
+    </div>
+  );
+};
+export default Clim;
